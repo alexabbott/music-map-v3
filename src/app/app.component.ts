51daa8ee8e4b662e0056c479ccf2041e540a9edc
Alex Abbott <alex.abbott@bynd.com>
2017-03-03 18:09:33 -0500

    Add base files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+  stations: FirebaseListObservable<any[]>;
+
+  constructor(af: AngularFire) {
+    this.stations = af.database.list('/stations');
+  }
+
+  onMapReady(map) {
+    console.log('map', map);
+    console.log('markers', map.markers);
+  }
+  onIdle(event) {
+    console.log('map', event.target);
+  }
+  onMarkerInit(marker) {
+    console.log('marker', marker);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { MaterialModule } from '@angular/material';
+import { Ng2MapModule} from 'ng2-map';
+import { Pipe, PipeTransform } from '@angular/core';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIrmC834Tivjh8EM0G2h8oUbC2gIye86o",
+  authDomain: "music-map-23635.firebaseapp.com",
+  databaseURL: "https://music-map-23635.firebaseio.com",
+  storageBucket: "music-map-23635.appspot.com",
+  messagingSenderId: "1038601253190"
+};
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    MaterialModule,
+    Ng2MapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyD9e_lkQIiKtphl0vGK3MjbC589jQcRtvk'})
   ],
   providers: [],
   bootstrap: [AppComponent]
