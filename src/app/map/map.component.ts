909ad717a1434b8909246f3687bddb4422bfd77d
Alex Abbott <alex.abbott@bynd.com>
2017-03-23 13:37:08 -0400

    Get marker clusters working

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs/Rx';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { GlobalService } from '../global.service';
+import '../../assets/markerclusterer.js';
 
 @Component({
   selector: 'map',
@@ -13,23 +14,53 @@ export class MapComponent {
   markerOptions;
   map;
   markers;
-  markerClusterer;
   newlocation: string;
   locations: FirebaseListObservable<any[]>;
   showReset: boolean;
+  googleMarkers: any;
+  google: any;
+  markerCount: number;
 
   constructor(public af: AngularFire, public globalService: GlobalService) {
     const me = this;
     this.locations = af.database.list('/location-stations');
     this.mapOptions = {
       zoom: 3,
+      center: {lat: 40.730610, lng: -73.935242 },
       mapTypeControl: false,
       streetViewControl: false,
       styles: [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.locality","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.neighborhood","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"off"},{"hue":"#ff0000"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#944242"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"},{"saturation":"-1"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#292929"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"},{"color":"#494949"},{"saturation":"-85"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#888888"},{"visibility":"on"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.bus","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.rail","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#dddddd"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]}]
     };
 
-    // this.markerClusterer = window['MarkerClusterer'];
-    // this.markers = [];
+    this.google = window['google'];
+    this.markers = [];
+
+    this.markerCount = 0;
+    this.locations.subscribe(locations => {
+      this.markerCount = locations.length;
+      console.log('markerCount', this.markerCount);
+      console.log('alllocations', locations);
+      for (let i = 0; i < this.markerCount; ++i ){
+        let coordinatesArray = locations[i][Object.keys(locations[i])[0]].coordinates.split(',');
+        this.markers.push({lat: parseInt(coordinatesArray[0]), lng: parseInt(coordinatesArray[1].trim())});
+      }
+      this.map = new google.maps.Map(document.getElementById('map'), this.mapOptions);
+      this.initMarkers();
+      var mc = new window['MarkerClusterer'](this.map, me.googleMarkers, { imagePath: 'https://googlemaps.github.io/js-marker-clusterer/images/m' } );
+    });
+
+  }
+
+  initMarkers(){
+    console.log('allmarkers', this.markers);
+    this.googleMarkers = [];
+    for (let i = 0; i < this.markerCount; ++i ){
+       this.googleMarkers.push( new google.maps.Marker({
+            position: this.markers[i],
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+        })
+       );
+    }
   }
 
   onMapReady(map) {
@@ -95,10 +126,5 @@ export class MapComponent {
       me.globalService.updateReset();
       me.globalService.updateHeadline(null);
     });
-
-    // let markerCluster = new this.markerClusterer(this.map, this.markers, {
-    //   imagePath: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-    // });
-    // console.log('markerclusterer', markerCluster);
   }
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { GlobalService } from '../global.service';
+import '../../assets/soundmanager2.js';
 
 @Component({
   selector: 'player',
