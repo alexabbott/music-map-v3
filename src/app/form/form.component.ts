25ff057da9cfc0afaa68c78f21fbce7cc5008833
Alex Abbott <alex.r.abbott@gmail.com>
2017-03-28 01:03:50 -0400

    Remove logs and update spacing of profile image

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -17,7 +17,6 @@ export class FormComponent {
   user;
   showForm: boolean;
   searchData;
-  searchRaw: any;
   searchResults: Array<any>;
   playlistKey: any;
   playlistTracks: Array<any>;
@@ -108,9 +107,7 @@ export class FormComponent {
       this.http.get('https://api.soundcloud.com/tracks?linked_partitioning=1&limit=30&client_id=' + this.clientId + '&q=' + keyword)
         .map(res => {
           res.text();
-          this.searchRaw = res.json();
           this.searchResults = res.json().collection;
-          console.log('results', this.searchRaw);
         })
         .subscribe(
           data => this.searchData = data,
